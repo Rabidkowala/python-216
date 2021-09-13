7dbc6b6bc69d9597f053e2f0193b525518f424cc
Rabidkowala <90586698+Rabidkowala@users.noreply.github.com>
2021-09-12 22:41:40 -0400

    Add files via upload

---
+++ Print Paragraph.py
@@ -0,0 +1,5 @@
+### Grant Choe
+### 9/12/21
+### CIS 216/ Python Programming
+
+print("Hello!  I'm taking this class to learn more coding.  I took CIS 150 last semester and enjoyed it immensely.  I'm hoping to extend my knowledge of python programming.  To be honest, I'm still too ignorant to really determine what I want to definitely learn but I hope to fill in the dark areas of my knowledge and find out that answer someday") ### I typed this paragraph from the heart (meaning it's literary vomit).  Very little planning went into it.  Took about 3 mins.  I'm not sure what "functions and classes must be documented with docstrings" means SO I'm hoping I did it by accident.  To use the program you just run it.  Once you do it should print out the paragraph above.  The program is very limited in function and operation and therefore very hard to mess up.  
